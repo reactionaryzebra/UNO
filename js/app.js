eb339b35def6be92d682f90a2e6c0d0c37e26245
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-04-03 17:10:11 -0700

    Add logic to toggle player type selectors on human selection modal

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 let deck;
+let numPlayers;
+const selectNumPlayers = document.getElementById('select-number-players')
+const selectHumanPlayers = document.getElementById('select-human-players')
 
 const game = {
   players: [],
@@ -9,6 +12,13 @@ const game = {
   init(){
     //Display graphic to allow User to select # players
     //Display graphic to allow User to slect # computer players
+    selectHumanPlayers.classList.toggle('visible')
+    if (numPlayers === 4){
+      document.getElementById('player-3-selector').classList.toggle('visible')
+      document.getElementById('player-4-selector').classList.toggle('visible')
+    } else if (numPlayers === 3){
+      document.getElementById('player-3-selector').classList.toggle('visible')
+    }
     //Display graphic to ask User their name
     //Set players array to players based on previous input
     //Display graphic to roll die to select seats
