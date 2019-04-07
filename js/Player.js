113fb2dd4e21a726016ffe8f26e2474d8817388c
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-04-07 12:24:33 -0700

    Add ComputerPlayer class

--- js/Player.js
+++ js/Player.js
@@ -23,3 +23,32 @@ class Player {
     }
   }
 }
+
+class ComputerPlayer extends Player {
+  constructor(name) {
+    super(name)
+    this.type = 'computer'
+  }
+  play() {
+    const legalCards = []
+    this.hand.forEach(card => {
+      if (card.isLegal){
+        legalCards.push(card)
+      }
+    })
+    const randomCardIndex = Math.floor(Math.random() * legalCards.length)
+    const cardToPlay = legalCards[randomCardIndex]
+    lastTurnDraw = false;
+    this.hand.splice(cardToPlay.handPosition, 1)
+    cardToPlay.handPosition = null
+    cardToPlay.isLegal = false
+    game.cardsInPlay.unshift(cardToPlay)
+    game.activeCard = game.cardsInPlay[0]
+    for (let i = 0; i < this.hand.length; i++) {
+      this.hand[i].handPosition = i
+    };
+    if (cardToPlay.value.includes('wild')){
+      //Handle Wilds
+    }
+  }
+}
